396016e1f3361cf006fee0c4803dda941d783ae3
phillip <phillipc@ziprecruiter.com>
2020-03-28 13:20:38 -0700

    start over

--- in/js/Main.js
+++
@@ -1,6 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Root = require('./components/Root')
-
-ReactDOM.render(<Root/>, document.getElementById('root'));
-// Bootstrap.renderTextNodes();
