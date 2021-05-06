b9bde9c17fe9df330f53cf47f16405e2d6c44330
phillip <phillipc@ziprecruiter.com>
2021-05-05 23:20:35 -0700

    Animate initial and looping card

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,1 +1,12 @@
-console.log('test');
+const getCardStack = () => (
+  document.querySelector('.card_stack')
+);
+
+document.querySelector('#test').addEventListener('click', () => {
+  getCardStack().classList.remove('initial');
+  getCardStack().classList.add('loop');
+});
+
+getCardStack().addEventListener('animationend', () => {
+  getCardStack().classList.remove('loop');
+});
